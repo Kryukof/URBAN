fb3a5b9344f9c84be881897d135a3754b976550a
Kryukof <167456405+Kryukof@users.noreply.github.com>
2024-07-11 16:11:02 +0500

    Update homework2.py

--- homework2.py
+++ homework2.py
@@ -1,6 +1,7 @@
 task = 12
 time = 1.5
 course = "Python"
+average_time = time / task
 print(f"Курс: {course}, всего задач: {task}, затрачено часов: {time}, среднее время выполнения "
-      f"{time / task} "
+      f"{average_time} "
       f"часа.")
