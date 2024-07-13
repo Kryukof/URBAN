1ca6a24bd757a2845a547e4027af41261d41b655
Kryukof <167456405+Kryukof@users.noreply.github.com>
2024-07-13 14:05:36 +0500

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,8 @@
+immutable_var = (1, 2, 3, "a", "b", True, False, 0.125)
+print(immutable_var)
+
+#immutable_var[0] = [200]  Является неизменяемым обьектом
+
+mutable_list = ([1, 2, 3], "a", "b")
+mutable_list[0][1] = 0
+print(mutable_list)
