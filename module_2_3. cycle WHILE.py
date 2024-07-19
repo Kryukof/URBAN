3c16077b6b4ffc22eb8fca28e6d0a2488f2d520e
Kryukof <167456405+Kryukof@users.noreply.github.com>
2024-07-19 10:45:45 +0500

    Add files via upload

---
+++ module_2_3. cycle WHILE.py
@@ -0,0 +1,15 @@
+my_list = [42, 69, 0, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+count = 0
+while len(my_list) > count:
+    print(my_list[count])
+    count += 1
+    if my_list[count] == 0:
+        count += 1
+    elif my_list[count] < 0:
+        print(f"Нашел отрицательное число: {my_list[count]}")
+        break
+
+
+
+
+
