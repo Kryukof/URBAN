c3fdfb95eafe609ccf9de92117e8aa4c2213a9a5
Kryukof <167456405+Kryukof@users.noreply.github.com>
2024-07-31 08:45:52 +0500

    Delete module_3_3.py

--- module_3_3.py
+++
@@ -1,8 +0,0 @@
-def print_params(a, b, c):
-     print(a, b, c)
-
-values_list = [False, "4, 5, 6", 7]
-values_dict = {"a": 7, "b": True, "c": "raketa"}
-values_list_2 = [54.32, "'Stroka'"]
-print_params(*values_list_2, 42)
-
